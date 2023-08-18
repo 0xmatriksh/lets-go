ff8799aa39dabce8a6b203398e53d67c38a80f31
0xmatriksh <kishmat17@gmail.com>
2023-08-18 20:44:50 +0545

    feat: gorilla/mux for routing, struct for books and get list of book endpoint

--- main.go
+++ main.go
@@ -1,17 +1,43 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request){
-	fmt.Fprint(w,"<h1>Hello Sir</h1>")
+type book struct {
+	Id       int
+	Title    string
+	Author   string
+	Quantity int
 }
 
-func main(){
-	http.HandleFunc("/", handlerFunc)
-	fmt.Println("Hello server!!!")
+var books = []book{
+	{Id: 1, Title: "A strange Loop", Author: "Alex Bradman", Quantity: 5},
+	{Id: 2, Title: "Atomic Habits", Author: "James Clear", Quantity: 7},
+	{Id: 3, Title: "Homo Sapiens", Author: "Yuval Noah Harari", Quantity: 2},
+}
 
-	http.ListenAndServe(":8080",nil)
+// GET: list of books
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	jsonResponse, err := json.Marshal(books)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err != nil {
+		return
+	}
+	w.Write(jsonResponse)
+}
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/", getBooks).Methods("GET")
+
+	http.Handle("/", router)
+
+	fmt.Println("Server Listening at http://127.0.0.1:8080/")
+	http.ListenAndServe(":8080", nil)
 }
